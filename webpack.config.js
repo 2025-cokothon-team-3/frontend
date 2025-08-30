ac3a4339a3b7bc5941d72e5013357d13e59b74fc
민제 <minje@minje-ui-MacBookAir.local>
2025-08-30 17:01:31 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+// webpack.config.mjs
+import path from 'path';
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: './client.jsx', // 또는 ./src/main.jsx 구조로 바꿔도 OK
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+    clean: true,
+    publicPath: '/',
+  },
+  resolve: { extensions: ['.js', '.jsx'] },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+              ['@babel/preset-react', { runtime: 'automatic' }],
+            ],
+          },
+        },
+      },
+      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+    }),
+  ],
+  devServer: {
+    static: { directory: path.join(__dirname, 'public') },
+    historyApiFallback: true,
+    hot: true,
+    port: 5173,
+    open: true,
+  },
+  devtool: 'source-map',
+};
