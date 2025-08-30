b82c7f7a19fb4afeec8f4abce0dbb61c5f659b1a
민제 <minjae5474@naver.com>
2025-08-31 02:18:35 +0900

    nickname api 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Question from './pages/Question1';
+import Question1 from './pages/Question1';
+import Question2 from './pages/Question2';
 import Result from './pages/Result';
 
 function App() {
@@ -8,7 +9,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/question1" element={<Question />} />
+        <Route path="/question1" element={<Question1 />} />
+        <Route path="/question2" element={<Question2 />} />
         <Route path="/result" element={<Result />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useEffect, useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import globeImg from '../assets/images/여행_마크.png';
@@ -28,13 +29,30 @@ function Home() {
     }, [nickname]);
 
     // 제출
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
         if (nickname.trim() === '') {
             alert('닉네임을 입력해주세요!');
             return;
         }
-        localStorage.setItem('nickname', nickname.trim());
-        navigate('/question1');
+
+        try {
+            const response = await axios.post('http://52.68.59.48:8081/api/users/login', {
+                nickname: nickname.trim(),
+            });
+
+            if (response.data.success) {
+                localStorage.setItem('nickname', nickname.trim());
+                navigate('/question1');
+            } else {
+                alert(response.data.message || '오류가 발생했습니다.');
+            }
+        } catch (error) {
+            if (error.response && error.response.data && error.response.data.message) {
+                alert(error.response.data.message);
+            } else {
+                alert('서버 오류가 발생했습니다.');
+            }
+        }
     };
 
     // 공유 링크 복사
